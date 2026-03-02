63ef33b5df832ee94a107466d6e6ce8e207f27e4
Tushar Shaw <tusharshaw961@gmail.com>
2026-03-02 22:46:42 +0530

    day-13 done

--- day-13/src/routes/auth.route.js
+++ day-13/src/routes/auth.route.js
@@ -2,6 +2,7 @@ const express=require("express");
 const authRouter=express.Router();
 const userModel=require("../model/user.model");
 const jwt=require("jsonwebtoken");
+const crypto = require("crypto");
 require("dotenv").config();
 
 
@@ -16,8 +17,10 @@ authRouter.post("/register",async (req,res)=>{
             message:"User with this email Already exists",
         });
     }
+
+    let hash=crypto.createHash("md5").update(password).digest("hex");
     let user=await userModel.create({
-        name,email,password
+        name,email,password:hash
     });
 
     const token=jwt.sign({
@@ -32,5 +35,35 @@ authRouter.post("/register",async (req,res)=>{
         user,token
     });
 });
+
+/*
+    LOGIN-REQUEST
+*/
+
+authRouter.post("/login",async (req,res)=>{
+    let { email , password }=req.body;
+    let user=await userModel.findOne({email});
+
+    if(!user){
+        res.status(404).json({
+            message:"This email is not registered"
+        })
+    }
+
+    let hash=crypto.createHash("md5").update(password).digest("hex");
+    if(hash != user.password){
+        res.status(401).json({
+            message:"Incorrect password"
+        })
+    }
+    let token=jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET);
+
+    res.cookie("jwt_token",token);
+    res.status(200).json({
+        message:"Request executed successfully"
+    })
+})
 
 module.exports=authRouter;
