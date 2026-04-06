3422a750ea3c4c422f8c119defc06ea644ce2feb
Tushar Shaw <tusharshaw961@gmail.com>
2026-04-06 15:31:52 +0530

    Mvp of the project has been created!!!

--- Ai-battle-Arena/src/app.ts
+++ Ai-battle-Arena/src/app.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import useGraph from "./services/graph.ai.service.js";
 
 const app = express();
 
 app.get("/health",(req,res)=>{
     res.status(200).json({message:"Server is healthy"});
+});
+
+app.post("/use-graph",async (req,res)=>{
+    await useGraph("Write a java program to find the factorial of a number");
+    res.status(200).json({message:"Graph invoked successfully"});
 });
 
 export default app;
--- Ai-battle-Arena/src/services/models.service.ts
+++ Ai-battle-Arena/src/services/models.service.ts
@@ -4,17 +4,26 @@ import { ChatCohere } from "@langchain/cohere"
 import config from "../config/config.js";
 
 
-export const geminiModel = new ChatGoogle({
+const geminiModel = new ChatGoogle({
     model: "gemini-flash-latest",
     apiKey: config.GOOGLE_API_KEY
 });
 
-export const mistralModel = new ChatMistralAI({
+const mistralModel = new ChatMistralAI({
     model:"mistral-medium-latest",
     apiKey: config.MISTRAL_API_KEY
 })
 
-export const cohereModel = new ChatCohere({
+const cohereModel = new ChatCohere({
     model: "command-a-03-2025",
     apiKey: config.COHERE_API_KEY
 })
+
+const models = {
+    geminiModel,
+    mistralModel,
+    cohereModel
+}
+
+export default models;
+
